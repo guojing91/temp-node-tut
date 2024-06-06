9b5aac5bc878688a69f55ee56564560205622240
GuoJing <guojing21@gmail.com>
2024-06-05 23:13:57 -0400

    first commit

---
+++ 10-fs-sync.js
@@ -0,0 +1,15 @@
+const { readFileSync, writeFileSync } = require('fs')
+// same as
+// const fs = require('fs')
+// fs.readFileSync
+console.log('start');
+const first = readFileSync('./content/first.txt', 'utf8')
+const second = readFileSync('./content/second.txt', 'utf8')
+
+console.log(first, second)
+
+writeFileSync('./content/result-sync.txt', `Here is the result : ${first}, ${second}`, { flag: 'a' })
+
+// { flag: 'a' } is to append
+console.log('done with this task')
+console.log('starting the next one')
